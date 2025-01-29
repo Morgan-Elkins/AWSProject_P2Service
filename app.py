ed27e466d15c6c8b5cb2511e92d8be894700b036
Morgan-Elkins <me839@york.ac.uk>
2025-01-29 14:29:17 +0000

    Example making Jira issue working

--- app.py
+++ app.py
@@ -5,12 +5,40 @@ import time
 from dotenv import load_dotenv
 import boto3
 from flask import Flask
+from jira import JIRA
 
 load_dotenv()
 
 AWS_REGION = os.getenv("AWS_REGION")
 AWS_QUEUE = os.getenv("AWS_Q2")
+JIRA_KEY = os.getenv("JIRA_KEY")
+EMAIL = os.getenv("EMAIL")
+HOST = os.getenv("HOST")
+
 sqs = boto3.client('sqs', region_name=AWS_REGION)
+
+jira = JIRA(server=HOST, basic_auth=(EMAIL, JIRA_KEY))
+
+# Issue data (replace with your own data)
+issue_data = {
+    "project": {"key": "MAPID"},
+    "summary": "New issue created via Python",
+    "description": "This is a sample issue created using Python script.",
+    "issuetype": {"name": "Task"},
+}
+
+MAPID = jira.project('MAPID')
+print(MAPID.name)
+
+try:
+    # Create the issue
+    new_issue = jira.create_issue(fields=issue_data)
+
+    # Print the key of the created issue
+    print("Issue created successfully!")
+    print("Issue Key:", new_issue.key)
+except Exception as e:
+    print("Failed to create issue:", str(e))
 
 def create_app():
     app = Flask(__name__)
@@ -24,7 +52,7 @@ def create_app():
 
 def get_messages():
     print("TEST")
-    print(f"{AWS_REGION} {AWS_QUEUE}")
+    print(f"{AWS_REGION} {AWS_QUEUE} {JIRA_KEY}")
     while True:
         try:
             response = sqs.receive_message(
@@ -61,9 +89,8 @@ def get_messages():
         time.sleep(1)
 
 
-
-
 if __name__ == '__main__':
     # app = create_app()
     # app.run()
-    get_messages()
+    # get_messages()
+    pass
